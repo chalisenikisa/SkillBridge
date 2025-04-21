f2ca662c4240d96e9a71993207f73df826f83c8c
chalisenikisa <nikisachalise99@gmail.com>
2025-04-21 18:34:50 +0545

    change in onlinecourse.sql

--- admin/student_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$conn = mysqli_connect("localhost", "username", "password", "database");
-
-$reg_no = $_POST['reg_no'];
-$password = $_POST['password'];
-
-$query = "SELECT * FROM login WHERE reg_no='$reg_no' AND password='$password'";
-$result = mysqli_query($conn, $query);
