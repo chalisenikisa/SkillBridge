ebe4b6c0d4bd9081504fb7a26153080cd475409f
chalisenikisa <nikisachalise99@gmail.com>
2025-04-29 20:50:21 +0545

    change in change-password.php

--- admin/change-password.php
+++ admin/change-password.php
@@ -1,29 +1,50 @@
 
 <?php
+// Start the session to track the logged-in user
 session_start();
-include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
-    {   
-header('location:index.php');
-}
-else{
-date_default_timezone_set('Asia/Kathmandu');// change according timezone
-$currentTime = date( 'd-m-Y h:i:s A', time () );
 
+// Include the configuration file for database connection
+include("includes/config.php");
 
-if(isset($_POST['submit']))
-{
-$sql=mysqli_query($con,"SELECT password FROM  admin where password='".md5($_POST['cpass'])."' && username='".$_SESSION['alogin']."'");
-$num=mysqli_fetch_array($sql);
-if($num>0)
-{
- $con=mysqli_query($con,"update admin set password='".md5($_POST['newpass'])."', updationDate='$currentTime' where username='".$_SESSION['alogin']."'");
-echo '<script>alert("Password Changed Successfully !!")</script>';
-echo '<script>window.location.href=change-password.php</script>';
-}else {
-echo '<script>alert("Old Password not match !!")</script>';
-echo '<script>window.location.href=change-password.php</script>';
-}
+// If the form is submitted, handle the password change logic
+if(isset($_POST['submit'])){
+    $currentPassword = $_POST['currentpassword']; // Current password entered by user
+    $newPassword = $_POST['newpassword']; // New password entered by user
+    $confirmPassword = $_POST['confirmpassword']; // Confirm new password entered by user
+
+    // Fetch the current password hash from the database for the logged-in user
+    $query = mysqli_query($con, "SELECT * FROM admin WHERE username='".$_SESSION['alogin']."'");
+    $num = mysqli_fetch_array($query);
+    
+    if($num) {
+        $storedHash = $num['password']; // The current hashed password in the database
+        
+        // Check if the current password entered by the user matches the stored hashed password
+        if(password_verify($currentPassword, $storedHash)) {
+            // Check if the new password and confirm password match
+            if($newPassword == $confirmPassword) {
+                // Hash the new password
+                $newHashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+                
+                // Update the password in the database
+                $updateQuery = mysqli_query($con, "UPDATE admin SET password='$newHashedPassword' WHERE username='".$_SESSION['alogin']."'");
+
+                if($updateQuery) {
+                    $_SESSION['msg'] = "Password updated successfully!";
+                    header("Location: login.php"); // Redirect to login page after successful password change
+                    exit();
+                } else {
+                    $_SESSION['errmsg'] = "Error updating password!";
+                }
+            } else {
+                $_SESSION['errmsg'] = "New password and confirm password do not match!";
+            }
+        } else {
+            $_SESSION['errmsg'] = "Current password is incorrect!";
+        }
+    } else {
+        $_SESSION['errmsg'] = "No user found!";
+    }
 }
 ?>
 
@@ -31,104 +52,70 @@ echo '<script>window.location.href=change-password.php</script>';
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title>Admin | Change Password</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Change Password</title>
     <link href="../assets/css/bootstrap.css" rel="stylesheet" />
     <link href="../assets/css/font-awesome.css" rel="stylesheet" />
     <link href="../assets/css/style.css" rel="stylesheet" />
 </head>
-<script type="text/javascript">
-function valid()
-{
-if(document.chngpwd.cpass.value=="")
-{
-alert("Current Password Filed is Empty !!");
-document.chngpwd.cpass.focus();
-return false;
-}
-else if(document.chngpwd.newpass.value=="")
-{
-alert("New Password Filed is Empty !!");
-document.chngpwd.newpass.focus();
-return false;
-}
-else if(document.chngpwd.cnfpass.value=="")
-{
-alert("Confirm Password Filed is Empty !!");
-document.chngpwd.cnfpass.focus();
-return false;
-}
-else if(document.chngpwd.newpass.value!= document.chngpwd.cnfpass.value)
-{
-alert("Password and Confirm Password Field do not match  !!");
-document.chngpwd.cnfpass.focus();
-return false;
-}
-return true;
-}
-</script>
 <body>
-<?php include('includes/header.php');?>
-    <!-- LOGO HEADER END-->
-<?php if($_SESSION['alogin']!="")
-{
- include('includes/menubar.php');
-}
- ?>
-    <!-- MENU SECTION END-->
+    <?php include('includes/header.php');?> <!-- Include the header -->
+
+    <section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                             <li><a href="../index.php">Home </a></li>
+                             <li><a href="index.php">Admin Login </a></li>
+                             <li><a href="../index.php">Student Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <div class="content-wrapper">
         <div class="container">
-              <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="page-head-line">Admin Change Password </h1>
-                    </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="page-head-line">Change Your Password</h4>
                 </div>
-                <div class="row" >
-                  <div class="col-md-3"></div>
+            </div>
+
+            <!-- Displaying success or error messages -->
+            <span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?></span>
+            <span style="color:green;"><?php echo htmlentities($_SESSION['msg']); ?></span>
+            
+            <!-- Password change form -->
+            <form name="changePassword" method="post">
+                <div class="row">
                     <div class="col-md-6">
-                        <div class="panel panel-default">
-                        <div class="panel-heading">
-                           Change Password
-                        </div>
-<font color="green" align="center"><?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?></font>
-
-
-                        <div class="panel-body">
-                       <form name="chngpwd" method="post" onSubmit="return valid();">
-   <div class="form-group">
-    <label for="exampleInputPassword1">Current Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="cpass" placeholder="Password" />
-  </div>
-   <div class="form-group">
-    <label for="exampleInputPassword1">New Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword2" name="newpass" placeholder="Password" />
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Confirm Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword3" name="cnfpass" placeholder="Password" />
-  </div>
- 
-  <button type="submit" name="submit" class="btn btn-default">Submit</button>
-                           <hr />
-   
-
-
-
-</form>
-                            </div>
-                            </div>
+                        <label>Enter Current Password: </label>
+                        <input type="password" name="currentpassword" class="form-control" required />
+                        
+                        <label>Enter New Password: </label>
+                        <input type="password" name="newpassword" class="form-control" required />
+                        
+                        <label>Confirm New Password: </label>
+                        <input type="password" name="confirmpassword" class="form-control" required />
+                        
+                        <hr />
+                        <button type="submit" name="submit" class="btn btn-info">Update Password</button>
                     </div>
-                  
                 </div>
+            </form>
         </div>
     </div>
-    <!-- CONTENT-WRAPPER SECTION END-->
-  <?php include('includes/footer.php');?>
-    <!-- FOOTER SECTION END-->
-    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY SCRIPTS -->
+
+    <?php include('includes/footer.php');?> <!-- Include the footer -->
+
     <script src="../assets/js/jquery-1.11.1.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
     <script src="../assets/js/bootstrap.js"></script>
 </body>
 </html>
-<?php } ?>
