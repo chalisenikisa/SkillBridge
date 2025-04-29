b58bec74cc4804b4e3af86e936c92215aa652c89
chalisenikisa <nikisachalise99@gmail.com>
2025-04-29 20:18:09 +0545

    changes in admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -2,27 +2,40 @@
 session_start();
 error_reporting(0);
 include("includes/config.php");
-if(isset($_POST['submit']))
-{
-    $username=$_POST['username'];
-    $password=md5($_POST['password']);
-$query=mysqli_query($con,"SELECT * FROM admin WHERE username='$username' and password='$password'");
-$num=mysqli_fetch_array($query);
-if($num>0)
-{
-$_SESSION['alogin']=$_POST['username'];
-$_SESSION['id']=$num['id'];
-header("location:change-password.php");
-exit();
-}
-else
-{
-$_SESSION['errmsg']="Invalid username or password";
-header("location:index.php");
-exit();
-}
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password']; // This will now be the plain password entered by the admin
+
+    // Query to fetch the admin by username
+    $query = mysqli_query($con, "SELECT * FROM admin WHERE username='$username'");
+    $num = mysqli_fetch_array($query);
+
+    if ($num) {
+        $storedHash = $num['password']; // The hashed password stored in the database
+        
+        // Use password_verify to check if the entered password matches the stored hash
+        if (password_verify($password, $storedHash)) {
+            // Password matched
+            $_SESSION['alogin'] = $_POST['username'];
+            $_SESSION['id'] = $num['id'];
+            header("location: change-password.php");
+            exit();
+        } else {
+            // Invalid password
+            $_SESSION['errmsg'] = "Invalid username or password";
+            header("location: index.php");
+            exit();
+        }
+    } else {
+        // No user found
+        $_SESSION['errmsg'] = "Invalid username or password";
+        header("location: index.php");
+        exit();
+    }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -49,45 +62,47 @@ exit();
                              <li><a href="../index.php">Home </a></li>
                              <li><a href="index.php">Admin Login </a></li>
                               <li><a href="../index.php">Student Login</a></li>
-        
-
                         </ul>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
+
     <div class="content-wrapper">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h4 class="page-head-line">Please Login To Enter in to Admin Panel </h4>
-
+                    <h4 class="page-head-line">Please Login To Enter into Admin Panel </h4>
                 </div>
-
             </div>
-             <span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
+
+            <span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg'] = ""); ?></span>
+
             <form name="admin" method="post">
-            <div class="row">
-                <div class="col-md-6">
-                     <label>Enter Username : </label>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label>Enter Username : </label>
                         <input type="text" name="username" class="form-control" required />
+
                         <label>Enter Password :  </label>
                         <input type="password" name="password" class="form-control" required />
+
                         <hr />
                         <button type="submit" name="submit" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> &nbsp;Log Me In </button>&nbsp;
+                    </div>
                 </div>
-                </form>
-                <div class="col-md-6">
-                 <img src="../assets/img/admin.png" class="img-responsive">
-                                    </div>
+            </form>
 
+            <div class="col-md-6">
+                <img src="../assets/img/admin.png" class="img-responsive" />
             </div>
         </div>
     </div>
+
     <!-- CONTENT-WRAPPER SECTION END-->
     <?php include('includes/footer.php');?>
+
     <!-- FOOTER SECTION END-->
     <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
     <!-- CORE JQUERY SCRIPTS -->
