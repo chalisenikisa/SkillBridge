b9ad1c1ef405a392c661db7c52c22024db010b68
chalisenikisa <nikisachalise99@gmail.com>
2025-05-01 11:58:58 +0545

    change in manage-students.php

--- admin/manage-students.php
+++ admin/manage-students.php
@@ -20,10 +20,10 @@ echo '<script>window.location.href=manage-students.php</script>';
 //Code for Password Rest
      if(isset($_GET['pass']))
       {
-        $password="name@123";
-        $newpass=md5($password);
+        $password="password";
+        $newpass=password_hash($newPassword, PASSWORD_DEFAULT);;
               mysqli_query($con,"update students set password='$newpass' where StudentRegno = '".$_GET['id']."'");
-              echo '<script>alert("Password Reset. New Password is name@123")</script>';
+              echo '<script>alert("Password Reset. New Password is password")</script>';
 echo '<script>window.location.href=manage-students.php</script>';
       } 
 ?>
