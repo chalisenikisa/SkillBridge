579eb2551edd7e8290ec1fd078f8ac57e27b069b
chalisenikisa <nikisachalise99@gmail.com>
2025-05-04 21:47:41 +0545

    changes in admin

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -1,31 +1,15 @@
-<nav class="navbar navbar-default sidebar" role="navigation">
-    <div class="container-fluid">
-        <!-- Sidebar toggle -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#admin-sidebar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php?page=dashboard">Admin Panel</a>
-        </div>
-
-        <!-- Sidebar content -->
-        <div class="collapse navbar-collapse" id="admin-sidebar">
-            <ul class="nav navbar-nav">
-                <li><a href="index.php?page=dashboard"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
-                <li><a href="index.php?page=session"><i class="glyphicon glyphicon-calendar"></i> Session</a></li>
-                <li><a href="index.php?page=semester"><i class="glyphicon glyphicon-time"></i> Semester</a></li>
-                <li><a href="index.php?page=department"><i class="glyphicon glyphicon-briefcase"></i> Department</a></li>
-                <li><a href="index.php?page=course"><i class="glyphicon glyphicon-book"></i> Course</a></li>
-                <li><a href="index.php?page=registration"><i class="glyphicon glyphicon-user"></i> Registration</a></li>
-                <li><a href="index.php?page=manage_students"><i class="glyphicon glyphicon-tasks"></i> Manage Students</a></li>
-                <li><a href="index.php?page=enroll_history"><i class="glyphicon glyphicon-list-alt"></i> Enroll History</a></li>
-                <li><a href="index.php?page=student_log"><i class="glyphicon glyphicon-file"></i> Student Logs</a></li>
-                <li><a href="index.php?page=news"><i class="glyphicon glyphicon-bullhorn"></i> News</a></li>
-                <li><a href="logout.php"><i class="glyphicon glyphicon-log-out"></i> Logout</a></li>
-            </ul>
-        </div>
-    </div>
+<nav>
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="index.php?page=index">INDEX</a></li>
+        <li><a href="index.php?page=session">SESSION</a></li>
+        <li><a href="index.php?page=semester">SEMESTER</a></li>
+        <li><a href="index.php?page=department">DEPARTMENT</a></li>
+        <li><a href="index.php?page=course">COURSE</a></li>
+        <li><a href="index.php?page=registration">REGISTRATION</a></li>
+        <li><a href="index.php?page=manage students">MANAGE STUDENTS</a></li>
+        <li><a href="index.php?page=enroll history">ENROLL HISTORY</a></li>
+        <li><a href="index.php?page=student log">Student LOGS</a></li>
+        <li><a href="index.php?page=news">NEWS</a></li>
+        <li><a href="index.php?page=logout">LOGOUT</a></li>
+    </ul>
 </nav>
