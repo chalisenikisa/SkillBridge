133cff7636a50dae0acb2a59081d1b73ae39224d
chalisenikisa <nikisachalise99@gmail.com>
2025-05-04 21:31:09 +0545

    changes in admin

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -1,10 +1,19 @@
-<nav>
-    <ul class="nav nav-pills nav-stacked">
-        <li><a href="dashboard.php">Dashboard</a></li>
-        <li><a href="enroll-course.php">Enroll for Course</a></li>
-        <li><a href="enroll-history.php">Enroll History</a></li>
-        <li><a href="my-profile.php">My Profile</a></li>
-        <li><a href="change-password.php">Change Password</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
-</nav>
+<section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                             <li><a href="pincode-verification.php">Enroll for Course </a></li>
+                             <li><a href="enroll-history.php">Enroll History  </a></li>
+                              <li><a href="my-profile.php">My Profile</a></li>
+                               <li><a href="change-password.php">Change Password</a></li>
+                            <li><a href="logout.php">Logout</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
