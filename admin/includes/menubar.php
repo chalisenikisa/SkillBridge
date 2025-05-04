4f3856556d8e9f7ffc9e85e9d6aa2bdf57cfdf08
chalisenikisa <nikisachalise99@gmail.com>
2025-05-04 21:24:26 +0545

    change in admain

---
+++ admin/includes/menubar.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="enroll-course.php">Enroll for Course</a></li>
+        <li><a href="enroll-history.php">Enroll History</a></li>
+        <li><a href="my-profile.php">My Profile</a></li>
+        <li><a href="change-password.php">Change Password</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
