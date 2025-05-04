d2b5593eed60858ed23b1e51ae08a42698c42545
chalisenikisa <nikisachalise99@gmail.com>
2025-05-04 18:18:24 +0545

    changes in sidebar.php

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,15 @@
+<nav>
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="index.php?page=index">INDEX</a></li>
+        <li><a href="index.php?page=session">SESSION</a></li>
+        <li><a href="index.php?page=semester">SEMESTER</a></li>
+        <li><a href="index.php?page=department">DEPARTMENT</a></li>
+        <li><a href="index.php?page=course">COURSE</a></li>
+        <li><a href="index.php?page=registration">REGISTRATION</a></li>
+        <li><a href="index.php?page=manage students">MANAGE STUDENTS</a></li>
+        <li><a href="index.php?page=enroll history">ENROLL HISTORY</a></li>
+        <li><a href="index.php?page=student log">Student LOGS</a></li>
+        <li><a href="index.php?page=news">NEWS</a></li>
+        <li><a href="index.php?page=logout">LOGOUT</a></li>
+    </ul>
+</nav>
