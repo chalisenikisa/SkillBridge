0a2ab743f8264a7d1f600cfffe578fa700782656
chalisenikisa <nikisachalise99@gmail.com>
2025-05-05 20:57:44 +0545

    changes in header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,25 +1,7 @@
-<?php
-error_reporting(0);
-?>
-
-    <!-- HEADER END-->
-    <div class="navbar navbar-inverse set-radius-zero">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                 <a class="navbar-brand" href="#" style="color:#fff; font-size:24px;4px; line-height:24px; ">
-
-                   Online Course Registration
-                </a>
-
-            </div>
-
-            <div class="left-div">
-                <i class="fa fa-user-plus login-icon" ></i>
-        </div>
-            </div>
-        </div>
+<div class="navbar-collapse collapse ">
+    <ul id="menu-top" class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="admin/index.php">Admin Login</a></li>
+        <li><a href="student/index.php">Student Login</a></li>
+    </ul>
+</div>
