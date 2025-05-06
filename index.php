c3691f7e352cc04b7949114e5232e937fa64aff2
chalisenikisa <nikisachalise99@gmail.com>
2025-05-06 16:34:16 +0545

    changes in student-dashboard.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 session_start();
 include('includes/config.php');
 
-// Check login
+// Redirect if not logged in
 if (!isset($_SESSION['login'])) {
-    header("Location: index.php");
+    header('location:index.php');
     exit();
 }
 
@@ -18,52 +18,53 @@ $studentName = $_SESSION['sname'];
     <title>Student Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSS -->
+    <!-- Bootstrap CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
+
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
 
 <?php include('includes/header.php'); ?>
 
-<div class="container" style="margin-top: 50px;">
+<div class="container" style="margin-top: 60px;">
     <div class="row">
-        <div class="col-md-12 text-center">
-            <h3>Welcome, <?php echo htmlentities($studentName); ?>!</h3>
+        <div class="col-md-12">
+            <h2 class="text-center">Welcome, <?php echo htmlentities($studentName); ?>!</h2>
             <hr>
         </div>
     </div>
 
-    <!-- Dashboard Icons -->
     <div class="row text-center">
         <div class="col-md-3 col-sm-6 mb-3">
-            <a href="enroll.php" class="btn btn-outline-primary btn-block">
-                <i class="fa fa-pencil-alt fa-2x"></i><br>Enroll for Course
+            <a href="enroll.php" class="btn btn-primary btn-block">
+                <i class="fa fa-pencil-alt"></i> Enroll for Course
             </a>
         </div>
 
         <div class="col-md-3 col-sm-6 mb-3">
-            <a href="enroll-history.php" class="btn btn-outline-info btn-block">
-                <i class="fa fa-history fa-2x"></i><br>Enroll History
+            <a href="enroll-history.php" class="btn btn-info btn-block">
+                <i class="fa fa-history"></i> Enroll History
             </a>
         </div>
 
         <div class="col-md-3 col-sm-6 mb-3">
-            <a href="my-profile.php" class="btn btn-outline-success btn-block">
-                <i class="fa fa-user fa-2x"></i><br>My Profile
+            <a href="my-profile.php" class="btn btn-success btn-block">
+                <i class="fa fa-user"></i> My Profile
             </a>
         </div>
 
         <div class="col-md-3 col-sm-6 mb-3">
-            <a href="change-password.php" class="btn btn-outline-warning btn-block">
-                <i class="fa fa-key fa-2x"></i><br>Change Password
+            <a href="change-password.php" class="btn btn-warning btn-block">
+                <i class="fa fa-key"></i> Change Password
             </a>
         </div>
 
         <div class="col-md-12 mt-4">
-            <a href="logout.php" class="btn btn-outline-danger btn-block">
-                <i class="fa fa-sign-out-alt fa-2x"></i><br>Logout
+            <a href="logout.php" class="btn btn-danger btn-block">
+                <i class="fa fa-sign-out-alt"></i> Logout
             </a>
         </div>
     </div>
@@ -71,7 +72,7 @@ $studentName = $_SESSION['sname'];
 
 <?php include('includes/footer.php'); ?>
 
-<!-- JS -->
+<!-- Scripts -->
 <script src="assets/js/jquery-1.11.1.js"></script>
 <script src="assets/js/bootstrap.js"></script>
 </body>
