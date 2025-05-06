d4e0ecb758f31963b8708776a6d20b7a0892a2c2
chalisenikisa <nikisachalise99@gmail.com>
2025-05-06 14:22:46 +0545

    changes in admin

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,36 +1,31 @@
-<div class="navbar-default sidebar" role="navigation" style="margin-top: 60px;">
+<!-- includes/sidebar.php -->
+<style>
+    .sidebar {
+        position: fixed;
+        top: 60px; /* Adjust based on your header height */
+        left: 0;
+        width: 220px;
+        height: 100%;
+        background-color: #f8f8f8;
+        overflow-y: auto;
+        border-right: 1px solid #e7e7e7;
+        padding-top: 20px;
+    }
+</style>
+
+<div class="navbar-default sidebar" role="navigation">
     <div class="sidebar-nav navbar-collapse">
         <ul class="nav" id="side-menu" style="padding: 15px;">
-            <li>
-                <a href="session.php"><i class="fa fa-calendar fa-fw"></i> Session</a>
-            </li>
-            <li>
-                <a href="semester.php"><i class="fa fa-book fa-fw"></i> Semester</a>
-            </li>
-            <li>
-                <a href="department.php"><i class="fa fa-sitemap fa-fw"></i> Department</a>
-            </li>
-            <li>
-                <a href="course.php"><i class="fa fa-book fa-fw"></i> Course</a>
-            </li>
-            <li>
-                <a href="student-registration.php"><i class="fa fa-user-plus fa-fw"></i> Registration</a>
-            </li>
-            <li>
-                <a href="manage-students.php"><i class="fa fa-users fa-fw"></i> Manage Students</a>
-            </li>
-            <li>
-                <a href="enroll-history.php"><i class="fa fa-history fa-fw"></i> Enroll History</a>
-            </li>
-            <li>
-                <a href="user-log.php"><i class="fa fa-file-text-o fa-fw"></i> Student Logs</a>
-            </li>
-            <li>
-                <a href="news.php"><i class="fa fa-newspaper-o fa-fw"></i> News</a>
-            </li>
-            <li>
-                <a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-            </li>
+            <li><a href="session.php"><i class="fa fa-calendar fa-fw"></i> Session</a></li>
+            <li><a href="semester.php"><i class="fa fa-book fa-fw"></i> Semester</a></li>
+            <li><a href="department.php"><i class="fa fa-sitemap fa-fw"></i> Department</a></li>
+            <li><a href="course.php"><i class="fa fa-book fa-fw"></i> Course</a></li>
+            <li><a href="student-registration.php"><i class="fa fa-user-plus fa-fw"></i> Registration</a></li>
+            <li><a href="manage-students.php"><i class="fa fa-users fa-fw"></i> Manage Students</a></li>
+            <li><a href="enroll-history.php"><i class="fa fa-history fa-fw"></i> Enroll History</a></li>
+            <li><a href="user-log.php"><i class="fa fa-file-text-o fa-fw"></i> Student Logs</a></li>
+            <li><a href="news.php"><i class="fa fa-newspaper-o fa-fw"></i> News</a></li>
+            <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
         </ul>
     </div>
 </div>
