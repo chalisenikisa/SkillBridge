2eac94cc1328732bcb2e74e07b2363898a6ee8e6
chalisenikisa <nikisachalise99@gmail.com>
2025-05-06 20:12:42 +0545

    changes in student

--- student/change-password.php
+++ student/change-password.php
@@ -9,7 +9,7 @@ if(isset($_POST['submit'])){
     $confirmPassword = $_POST['confirmpassword']; // Confirm new password entered by user
 
     // Fetch the current password hash from the database for the logged-in user
-    $query = mysqli_query($con, "SELECT * FROM admin WHERE username='".$_SESSION['alogin']."'");
+    $query = mysqli_query($con, "SELECT * FROM admin WHERE username='".$_SESSION['alogin']."'"); 
     $num = mysqli_fetch_array($query);
     
     if($num) {
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])){
                 
                 // Update the password in the database
                 $updateQuery = mysqli_query($con, "UPDATE admin SET password='$newHashedPassword' WHERE username='".$_SESSION['alogin']."'");
-                
+
                 if($updateQuery) {
                     $_SESSION['msg'] = "Password updated successfully!";
                 } else {
@@ -56,49 +56,59 @@ if(isset($_POST['submit'])){
     <link href="../assets/css/style.css" rel="stylesheet" />
 </head>
 <body>
+
     <?php include('includes/header.php');?>
 
-    <section class="menu-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="navbar-collapse collapse ">
-                        <ul id="menu-top" class="nav navbar-nav navbar-right">
-                              <li><a href="student/index.php">Student Login</a></li>
-                        </ul>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <!-- Sidebar -->
+            <div class="col-md-3">
+                <div class="sidebar" style="width: 220px; background-color: #f8f9fa; position: fixed; top: 0; bottom: 0; left: 0; overflow-y: auto; border-right: 1px solid #ddd; padding-top: 20px;">
+                    <a href="enroll.php" style="display: flex; align-items: center; padding: 12px 20px; color: #333; text-decoration: none;">
+                        <i class="fa fa-pencil-square-o" style="margin-right: 10px;"></i> Enroll for Course
+                    </a>
+                    <a href="enroll-history.php" style="display: flex; align-items: center; padding: 12px 20px; color: #333; text-decoration: none;">
+                        <i class="fa fa-history" style="margin-right: 10px;"></i> Enroll History
+                    </a>
+                    <a href="my-profile.php" style="display: flex; align-items: center; padding: 12px 20px; color: #333; text-decoration: none;">
+                        <i class="fa fa-user" style="margin-right: 10px;"></i> My Profile
+                    </a>
+                    <a href="change-password.php" style="display: flex; align-items: center; padding: 12px 20px; color: #333; text-decoration: none;">
+                        <i class="fa fa-lock" style="margin-right: 10px;"></i> Change Password
+                    </a>
+                    <a href="logout.php" style="display: flex; align-items: center; padding: 12px 20px; color: #333; text-decoration: none;">
+                        <i class="fa fa-sign-out" style="margin-right: 10px;"></i> Logout
+                    </a>
                 </div>
             </div>
-        </div>
-    </section>
 
-    <div class="content-wrapper">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h4 class="page-head-line">Change Your Password</h4>
-                </div>
-            </div>
-            <span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg'] = ""); ?></span>
-            <span style="color:green;"><?php echo htmlentities($_SESSION['msg']); ?><?php echo htmlentities($_SESSION['msg'] = ""); ?></span>
-            
-            <form name="changePassword" method="post">
-                <div class="row">
-                    <div class="col-md-6">
-                        <label>Enter Current Password: </label>
-                        <input type="password" name="currentpassword" class="form-control" required />
-                        
-                        <label>Enter New Password: </label>
-                        <input type="password" name="newpassword" class="form-control" required />
-                        
-                        <label>Confirm New Password: </label>
-                        <input type="password" name="confirmpassword" class="form-control" required />
-                        
-                        <hr />
-                        <button type="submit" name="submit" class="btn btn-info">Update Password</button>
+            <!-- Main content -->
+            <div class="col-md-9" style="margin-left: 240px;">
+                <h4 class="page-head-line">Change Your Password</h4>
+
+                <!-- Display error or success messages -->
+                <span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg'] = ""); ?></span>
+                <span style="color:green;"><?php echo htmlentities($_SESSION['msg']); ?><?php echo htmlentities($_SESSION['msg'] = ""); ?></span>
+                
+                <!-- Change password form -->
+                <form name="changePassword" method="post">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label>Enter Current Password: </label>
+                            <input type="password" name="currentpassword" class="form-control" required />
+                            
+                            <label>Enter New Password: </label>
+                            <input type="password" name="newpassword" class="form-control" required />
+                            
+                            <label>Confirm New Password: </label>
+                            <input type="password" name="confirmpassword" class="form-control" required />
+                            
+                            <hr />
+                            <button type="submit" name="submit" class="btn btn-info">Update Password</button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 
