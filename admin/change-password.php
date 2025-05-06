0153ccfbb280e64fe650c9d6d75a3573b826de4d
chalisenikisa <nikisachalise99@gmail.com>
2025-05-06 14:01:09 +0545

    changes in admin

--- admin/change-password.php
+++ admin/change-password.php
@@ -62,24 +62,7 @@ if (isset($_POST['submit'])) {
     <div class="row">
         
         <div class="col-md-3">
-            <nav>
             
-            <ul class="nav nav-pills nav-stacked">
-    
-    
-    <li><a href="session.php"><i class="fas fa-home"></i> SESSION</a></li>
-    <li><a href="semester.php"><i class="fas fa-calendar-alt"></i> SEMESTER</a></li>
-    <li><a href="department.php"><i class="fas fa-building"></i> DEPARTMENT</a></li>
-    <li><a href="course.php"><i class="fas fa-book"></i> COURSE</a></li>
-    <li><a href="student-registration.php"><i class="fas fa-edit"></i> REGISTRATION</a></li>
-    <li><a href="manage-students.php"><i class="fas fa-users"></i> MANAGE STUDENTS</a></li>
-    <li><a href="enroll-history.php"><i class="fas fa-history"></i> ENROLL HISTORY</a></li>
-    <li><a href="user-log.php"><i class="fas fa-clipboard-list"></i> STUDENT LOGS</a></li>
-    <li><a href="news.php"><i class="fas fa-newspaper"></i> NEWS</a></li>
-    <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> LOGOUT</a></li>
-</ul>
-
-            </nav>
         </div>
 
         <!-- Main content -->
