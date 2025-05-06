925588a77280e54c95ec355b0395323e227547bb
chalisenikisa <nikisachalise99@gmail.com>
2025-05-06 16:48:29 +0545

    changes in students

---
+++ students/include/footer.php
@@ -0,0 +1,6 @@
+<!-- includes/footer.php -->
+<footer class="bg-dark text-white text-center py-3 mt-5">
+    <div class="container">
+        &copy; <?php echo date("Y"); ?> SKILLBRIDGE | All Rights Reserved.
+    </div>
+</footer>
