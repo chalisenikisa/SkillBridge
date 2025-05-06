8dd332fc829d48a80eaff2da3cfdae84e3d1a6fe
chalisenikisa <nikisachalise99@gmail.com>
2025-05-06 17:12:43 +0545

    changes in SQL File

--- SQL File/onlinecourse (10).sql
+++ SQL File/onlinecourse (10).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2025 at 04:29 PM
+-- Generation Time: May 06, 2025 at 01:25 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
