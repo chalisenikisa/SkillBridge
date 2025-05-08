a0e44f425860378a06e2ccba95b850c166334147
chalisenikisa <nikisachalise99@gmail.com>
2025-05-08 14:08:24 +0545

    changes in student

--- student/change-password.php
+++ student/change-password.php
@@ -7,7 +7,7 @@ if (strlen($_SESSION['login']) == 0) {
     header('location:index.php');
     exit();
 } else {
-    date_default_timezone_set('Asia/Kathmandu');
+    date_default_timezone_set('Asia/Kolkata');
     $currentTime = date('d-m-Y h:i:s A', time());
 
     if (isset($_POST['submit'])) {
@@ -44,21 +44,50 @@ if (strlen($_SESSION['login']) == 0) {
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
+
     <style>
-        .sidebar {
-            background-color: #f5f5f5;
-            padding: 15px;
-            border-right: 1px solid #ddd;
+        * {
+            box-sizing: border-box;
+        }
+        body, html {
+            margin: 0;
+            padding: 0;
             height: 100%;
+        }
+        .wrapper {
+            display: flex;
+            height: calc(100vh - 70px); /* adjust if your header height changes */
+        }
+        .sidebar {
+            width: 220px;
+            background-color: #f8f9fa;
+            padding: 20px;
+            border-right: 1px solid #ddd;
+            flex-shrink: 0;
+        }
+        .sidebar h4 {
+            margin-top: 0;
         }
         .sidebar a {
             display: block;
-            padding: 8px 0;
+            padding: 10px 5px;
             color: #333;
             text-decoration: none;
         }
         .sidebar a:hover {
-            text-decoration: underline;
+            background-color: #e9ecef;
+        }
+        .content {
+            flex: 1;
+            padding: 40px;
+            background-color: #f5f5f5;
+            overflow-y: auto;
+        }
+        .page-head-line {
+            font-size: 24px;
+            border-bottom: 2px solid #ccc;
+            padding-bottom: 10px;
+            margin-bottom: 20px;
         }
     </style>
 
@@ -86,64 +115,59 @@ if (strlen($_SESSION['login']) == 0) {
     </script>
 </head>
 <body>
-<?php include('includes/header.php'); ?>
-<?php if ($_SESSION['login'] != "") include('includes/sidebar.php'); ?>
+    <?php include('includes/header.php'); ?>
+    <?php if ($_SESSION['login'] != "") include('includes/menubar.php'); ?>
 
-<div class="content-wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <!-- Sidebar -->
-            <div class="col-md-3">
-                <div class="sidebar">
-                    <h4>Student Menu</h4>
-                    <a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a>
-                    <a href="profile.php"><i class="fa fa-user"></i> Profile</a>
-                    <a href="enroll-course.php"><i class="fa fa-book"></i> Enroll Courses</a>
-                    <a href="my-courses.php"><i class="fa fa-list"></i> My Courses</a>
-                    <a href="change-password.php"><i class="fa fa-lock"></i> Change Password</a>
-                    <a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <h4>Student Menu</h4>
+            <a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a>
+            <a href="profile.php"><i class="fa fa-user"></i> Profile</a>
+            <a href="enroll-course.php"><i class="fa fa-book"></i> Enroll Courses</a>
+            <a href="my-courses.php"><i class="fa fa-list"></i> My Courses</a>
+            <a href="change-password.php"><i class="fa fa-lock"></i> Change Password</a>
+            <a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
+        </div>
+
+        <!-- Main Content -->
+        <div class="content">
+            <h1 class="page-head-line">Change Password</h1>
+
+            <?php if (!empty($_SESSION['msg'])): ?>
+                <div class="alert alert-info"><?php echo htmlentities($_SESSION['msg']); unset($_SESSION['msg']); ?></div>
+            <?php endif; ?>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Change Password</div>
+                <div class="panel-body">
+                    <form name="chngpwd" method="post" onsubmit="return valid();">
+                        <div class="form-group">
+                            <label for="cpass">Current Password</label>
+                            <input type="password" class="form-control" name="cpass" id="cpass" placeholder="Current Password" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="newpass">New Password</label>
+                            <input type="password" class="form-control" name="newpass" id="newpass" placeholder="New Password" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="cnfpass">Confirm Password</label>
+                            <input type="password" class="form-control" name="cnfpass" id="cnfpass" placeholder="Confirm Password" />
+                        </div>
+
+                        <button type="submit" name="submit" class="btn btn-primary">Change Password</button>
+                        <hr />
+                    </form>
                 </div>
             </div>
+        </div>
+    </div>
 
-            <!-- Main Content -->
-            <div class="col-md-9">
-                <h1 class="page-head-line">Change Password</h1>
+    <?php include('includes/footer.php'); ?>
 
-                <?php if (!empty($_SESSION['msg'])): ?>
-                    <div class="alert alert-info"><?php echo htmlentities($_SESSION['msg']); unset($_SESSION['msg']); ?></div>
-                <?php endif; ?>
-
-                <div class="panel panel-default">
-                    <div class="panel-heading">Change Password</div>
-                    <div class="panel-body">
-                        <form name="chngpwd" method="post" onsubmit="return valid();">
-                            <div class="form-group">
-                                <label for="cpass">Current Password</label>
-                                <input type="password" class="form-control" name="cpass" id="cpass" placeholder="Current Password" />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="newpass">New Password</label>
-                                <input type="password" class="form-control" name="newpass" id="newpass" placeholder="New Password" />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="cnfpass">Confirm Password</label>
-                                <input type="password" class="form-control" name="cnfpass" id="cnfpass" placeholder="Confirm Password" />
-                            </div>
-
-                            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-                            <hr />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- /.row -->
-    </div> <!-- /.container-fluid -->
-</div> <!-- /.content-wrapper -->
-
-<?php include('includes/footer.php'); ?>
-<script src="assets/js/jquery-1.11.1.js"></script>
-<script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
