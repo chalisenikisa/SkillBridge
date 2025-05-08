337b6454dd40435d56a384b5e675e07bec5e201f
chalisenikisa <nikisachalise99@gmail.com>
2025-05-08 18:34:45 +0545

    changes in student

--- student/enroll.php
+++ student/enroll.php
@@ -17,86 +17,76 @@ if (isset($_POST['submit'])) {
     $course = $_POST['course'];
     $sem = $_POST['sem'];
 
-    $stmt = $con->prepare("INSERT INTO courseenrolls (studentRegno, pincode, session, department, level, course, semester) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssiiiii", $studentregno, $pincode, $session, $dept, $level, $course, $sem);
+    $stmt = $con->prepare("INSERT INTO courseenrolls(studentRegno, pincode, session, department, level, course, semester) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssss", $studentregno, $pincode, $session, $dept, $level, $course, $sem);
     
     if ($stmt->execute()) {
-        echo "<script>alert('Enroll Successfully !!'); window.location.href='enroll.php';</script>";
+        echo '<script>alert("Enrolled Successfully!"); window.location.href="enroll.php";</script>';
     } else {
-        echo "<script>alert('Error: Not Enrolled'); window.location.href='enroll.php';</script>";
+        echo '<script>alert("Error: Not Enrolled."); window.location.href="enroll.php";</script>';
     }
     $stmt->close();
 }
 ?>
 
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
     <meta charset="utf-8" />
     <title>Course Enroll</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="../assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="../assets/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
-<?php include('includes/header.php'); ?>
+    <?php include('includes/header.php'); ?>
+    <?php if ($_SESSION['login'] != "") include('includes/menubar.php'); ?>
 
-<?php if ($_SESSION['login'] != "") {
-    include('includes/sidebar.php');
-} ?>
-
-<div class="content-wrapper">
-    <div class="container">
-
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="page-head-line">Course Enroll</h1>
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="page-head-line">Course Enroll</h1>
+                </div>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-3"></div>
-
-            <div class="col-md-6">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Course Enroll</div>
-
-                    <div class="panel-body">
-                        <form method="post" enctype="multipart/form-data">
-
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Enroll Form</div>
+                        <div class="panel-body">
                             <?php
                             $sql = mysqli_query($con, "SELECT * FROM students WHERE StudentRegno='" . $_SESSION['login'] . "'");
                             while ($row = mysqli_fetch_array($sql)) {
                             ?>
-                                <div class="form-group">
-                                    <label for="studentname">Student Name</label>
-                                    <input type="text" class="form-control" name="studentname" value="<?php echo htmlentities($row['studentName']); ?>" readonly />
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="studentregno">Student Reg No</label>
-                                    <input type="text" class="form-control" name="studentregno" value="<?php echo htmlentities($row['StudentRegno']); ?>" readonly />
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="Pincode">Pincode</label>
-                                    <input type="text" class="form-control" name="Pincode" value="<?php echo htmlentities($row['pincode']); ?>" readonly required />
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="Photo">Student Photo</label><br />
-                                    <?php if ($row['studentPhoto'] == "") { ?>
-                                        <img src="studentphoto/noimage.png" width="200" height="200" />
-                                    <?php } else { ?>
-                                        <img src="studentphoto/<?php echo htmlentities($row['studentPhoto']); ?>" width="200" height="200" />
-                                    <?php } ?>
-                                </div>
+                                <form name="enroll" method="post">
+                                    <div class="form-group">
+                                        <label>Student Name</label>
+                                        <input type="text" class="form-control" value="<?php echo htmlentities($row['studentName']); ?>" readonly />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Student Reg No</label>
+                                        <input type="text" class="form-control" name="studentregno" value="<?php echo htmlentities($row['StudentRegno']); ?>" readonly />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Pincode</label>
+                                        <input type="text" class="form-control" name="Pincode" value="<?php echo htmlentities($row['pincode']); ?>" readonly />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Student Photo</label><br>
+                                        <?php if ($row['studentPhoto'] == "") { ?>
+                                            <img src="studentphoto/noimage.png" width="200" height="200">
+                                        <?php } else { ?>
+                                            <img src="studentphoto/<?php echo htmlentities($row['studentPhoto']); ?>" width="200" height="200">
+                                        <?php } ?>
+                                    </div>
                             <?php } ?>
 
                             <div class="form-group">
-                                <label for="Session">Session</label>
+                                <label>Session</label>
                                 <select class="form-control" name="session" required>
                                     <option value="">Select Session</option>
                                     <?php
@@ -109,7 +99,7 @@ if (isset($_POST['submit'])) {
                             </div>
 
                             <div class="form-group">
-                                <label for="Department">Department</label>
+                                <label>Department</label>
                                 <select class="form-control" name="department" required>
                                     <option value="">Select Department</option>
                                     <?php
@@ -122,7 +112,7 @@ if (isset($_POST['submit'])) {
                             </div>
 
                             <div class="form-group">
-                                <label for="Level">Level</label>
+                                <label>Level</label>
                                 <select class="form-control" name="level" required>
                                     <option value="">Select Level</option>
                                     <?php
@@ -135,7 +125,7 @@ if (isset($_POST['submit'])) {
                             </div>
 
                             <div class="form-group">
-                                <label for="Semester">Semester</label>
+                                <label>Semester</label>
                                 <select class="form-control" name="sem" required>
                                     <option value="">Select Semester</option>
                                     <?php
@@ -148,8 +138,8 @@ if (isset($_POST['submit'])) {
                             </div>
 
                             <div class="form-group">
-                                <label for="Course">Course</label>
-                                <select class="form-control" name="course" id="course" onBlur="courseAvailability()" required>
+                                <label>Course</label>
+                                <select class="form-control" name="course" id="course" onblur="courseAvailability()" required>
                                     <option value="">Select Course</option>
                                     <?php
                                     $sql = mysqli_query($con, "SELECT * FROM course");
@@ -158,40 +148,34 @@ if (isset($_POST['submit'])) {
                                         <option value="<?php echo htmlentities($row['id']); ?>"><?php echo htmlentities($row['courseName']); ?></option>
                                     <?php } ?>
                                 </select>
-                                <span id="course-availability-status1" style="font-size: 12px;"></span>
+                                <span id="course-availability-status1" style="font-size:12px;"></span>
                             </div>
 
                             <button type="submit" name="submit" class="btn btn-primary">Enroll</button>
-
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- content-wrapper -->
 
-        </div>
+    <?php include('includes/footer.php'); ?>
 
-    </div>
-</div>
-
-<?php include('includes/footer.php'); ?>
-
-<script src="assets/js/jquery-1.11.1.js"></script>
-<script src="assets/js/bootstrap.js"></script>
-<script>
-function courseAvailability() {
-    $("#loaderIcon").show();
-    jQuery.ajax({
-        url: "check_availability.php",
-        data: 'cid=' + $("#course").val(),
-        type: "POST",
-        success: function (data) {
-            $("#course-availability-status1").html(data);
-            $("#loaderIcon").hide();
-        },
-        error: function () { }
-    });
-}
-</script>
-
+    <script src="../assets/js/jquery-1.11.1.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+    <script>
+        function courseAvailability() {
+            jQuery.ajax({
+                url: "check_availability.php",
+                data: 'cid=' + $("#course").val(),
+                type: "POST",
+                success: function (data) {
+                    $("#course-availability-status1").html(data);
+                },
+                error: function () { }
+            });
+        }
+    </script>
 </body>
 </html>
