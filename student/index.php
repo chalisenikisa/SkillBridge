d1b739d86e29916be0d299904ef26546e2256aa7
chalisenikisa <nikisachalise99@gmail.com>
2025-05-19 20:25:40 +0545

    changes in student/index.php

--- student/index.php
+++ student/index.php
@@ -37,6 +37,7 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Student Login</title>
     
 
